04230deaf9fe200806f2d347d52bcb35fce10436
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-14 18:04:33 -0300

    AdsenseController: ajustes de identação. Adição do JavaDocs aos novos métodos

--- src/main/java/br/com/meli/desafio_final/controller/AdsenseController.java
+++ src/main/java/br/com/meli/desafio_final/controller/AdsenseController.java
@@ -1,6 +1,5 @@
 package br.com.meli.desafio_final.controller;
 
-
 import br.com.meli.desafio_final.dto.*;
 import br.com.meli.desafio_final.model.entity.Adsense;
 import br.com.meli.desafio_final.model.enums.Category;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -21,9 +19,7 @@ public class AdsenseController {
 
     /**
      * Nesse método retornamos uma lista de anúncios
-     * @return
      */
-    // TODO: colocar DTO
     @GetMapping
     public ResponseEntity<List<AdsenseDto>> findAll() {
         return ResponseEntity.ok(AdsenseDto.convertListDto(adsenseService.findAll()));
@@ -32,9 +28,7 @@ public class AdsenseController {
     /**
      * Nesse método retornamos anúncio listado por categoria
      * @param querytype
-     * @return
      */
-    // TODO: colocar DTO
     @GetMapping("/list")
     public ResponseEntity<List<AdsenseDto>> findByCategory(@RequestParam Category querytype) {
         return ResponseEntity.ok(AdsenseDto.convertListDto(adsenseService.findByCategory(querytype)));
@@ -43,33 +37,72 @@ public class AdsenseController {
     /**
      * Nesse método retornamos um lista de anúncio por armazém / ou a qual armazém esse anúncio pertece
      * @param adsenseId
-     * @return
      */
     @GetMapping("/warehouse/{adsenseId}")
     public ResponseEntity <List<AdsenseByWarehouseDto>> getByAdsenseByWarehouse(@PathVariable Long adsenseId) {
         return ResponseEntity.status(HttpStatus.OK).body(adsenseService.findAdsenseByWarehouseAndQuantity(adsenseId));
     }
 
-
+    /**
+     * Método responsável pela criação de um novo anúncio. Retorna um ResponseEntity contendo o status da requisição e
+     * no corpo os dados do anúncio criado.
+     * @param newAdsense Os dados do anúncio informados na requisção
+     * @return <ul>
+     *     <li>ResponseEntity</li>
+     *     <li>HttpStatus.CREATED</li>
+     *     <li>Body</li>
+     * </ul>
+     * @author Letícia Castro
+     */
     @PostMapping("/insert")
     public ResponseEntity<AdsenseInsertDto> createAdsense(@RequestBody Adsense newAdsense) {
         return ResponseEntity.status(HttpStatus.CREATED).body(adsenseService.insertAdsense(newAdsense));
     }
 
+    /**
+     * Método responsável por listar um anúncio específico. Retorna um ResponseEntity contendo o status da requisição e
+     * no corpo os dados do anúncio se encontrado.
+     * @param adsenseId O ID do anúncio a ser pesquisado no banco.
+     * @return <ul>
+     *     <li>ResponseEntity</li>
+     *     <li>HttpStatus.OK</li>
+     *     <li>Body</li>
+     * </ul>
+     * @author Letícia Castro
+     */
     @GetMapping("/{adsenseId}")
     public ResponseEntity<AdsenseDto> readAdsenseById(@PathVariable Long adsenseId) {
         return ResponseEntity.status(HttpStatus.OK).body(AdsenseDto.convertDto(adsenseService.findById(adsenseId)));
     }
 
+    /**
+     * Método responsável por atualizar um anúncio específico de um determinado vendedor.
+     * Retorna um ResponseEntity contendo o status da requisição
+     * e no corpo os dados do anúncio atualizado.
+     * @param adsenseId O ID do anúncio a ser pesquisado no banco.
+     * @param sellerId O ID do vendedor.
+     * @param adsense Os novos dados do anúncio.
+     * @return <ul>
+     *     <li>ResponseEntity</li>
+     *     <li>HttpStatus.OK</li>
+     *     <li>Body</li>
+     * </ul>
+     * @author Letícia Castro
+     */
     @PutMapping("/{adsenseId}")
-    public ResponseEntity<AdsenseUpdateDto> updateAdsense(
-        @RequestBody Adsense adsense,
-        @PathVariable Long adsenseId,
-        @RequestParam Long sellerId) {
-        return ResponseEntity.status(HttpStatus.OK).body(
-            adsenseService.updateAdsenseById(adsense, adsenseId, sellerId));
+    public ResponseEntity<AdsenseUpdateDto> updateAdsense(@RequestBody Adsense adsense,
+        @PathVariable Long adsenseId, @RequestParam Long sellerId) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(adsenseService.updateAdsenseById(adsense, adsenseId, sellerId));
     }
 
+    /**
+     * Método responsável por apagar um anúncio específico.
+     * @param adsenseId O ID do anúncio a ser pesquisado no banco.
+     * @return HttpStatus.NO_CONTENT
+     * @author Letícia Castro
+     */
     @DeleteMapping("/{adsenseId}")
     public ResponseEntity<Void> deleteAdsense(@PathVariable Long adsenseId) {
         this.adsenseService.deleteAdsenseById(adsenseId);
