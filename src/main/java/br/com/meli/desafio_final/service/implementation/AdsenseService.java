473ed8cff003fc6accc4996e4b4aefc4fe71167c
Letícia Castro <leticia.castro@mercadolivre.com>
2022-08-15 12:53:26 -0300

    AdsenseService: Removidos imports não utilizados. Ajustes de identação. Adição de JavaDocs dos métodos do requisito 06

--- src/main/java/br/com/meli/desafio_final/service/implementation/AdsenseService.java
+++ src/main/java/br/com/meli/desafio_final/service/implementation/AdsenseService.java
@@ -10,15 +10,12 @@ import br.com.meli.desafio_final.model.entity.Product;
 import br.com.meli.desafio_final.model.entity.Seller;
 import br.com.meli.desafio_final.model.enums.Category;
 import br.com.meli.desafio_final.repository.AdsenseRepository;
-import br.com.meli.desafio_final.repository.ProductRepository;
 import br.com.meli.desafio_final.repository.SellerRepository;
-import br.com.meli.desafio_final.service.implementation.BatchService;
 import br.com.meli.desafio_final.service.IAdsenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.meli.desafio_final.dto.AdsenseIdDto;
 
-import javax.validation.constraints.Null;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -41,29 +38,29 @@ public class AdsenseService implements IAdsenseService {
     /**
      * Nesse método estamos retornado/ consultando anúncio Id
      * @param id
-     * @return
      */
     @Override
     public Adsense findById(long id) {
         return adsenseRepository.findById(id)
-                .orElseThrow(() -> { throw new NotFound("Anúncio não cadastrado."); } );
+            .orElseThrow(() -> { throw new NotFound("💢 Anúncio não cadastrado."); });
     }
 
     /**
-     * Nesse método consultamos uma lista de anúncios e retornamos lista caso existe, caso não exibimos uma mensagem de erro
-     * @return
+     * Nesse método consultamos uma lista de anúncios e retornamos lista caso existe,
+     * caso não exibimos uma mensagem de erro
      */
     @Override
     public List<Adsense> findAll() {
         List<Adsense> adsenses = adsenseRepository.findAll();
+
         if (adsenses.size() == 0) throw new NotFound("💢 Lista de anúncios não encontrada");
+
         return adsenses;
     }
 
     /**
      * Nesse método retornamos uma lista de anúncio filtrado por categoria
      * @param category
-     * @return
      */
     @Override
     public List<Adsense> findByCategory(Category category) {
@@ -72,7 +69,8 @@ public class AdsenseService implements IAdsenseService {
             .filter(a -> a.getProduct().getCategory().equals(category))
             .collect(Collectors.toList());
 
-        if (adsenseList.isEmpty()) throw new NotFound("Não existem anuncios com essa categoria");
+        if (adsenseList.isEmpty()) throw new NotFound("💢 Não existem anúncios com essa categoria");
+
         return adsenseList;
     }
     // TODO: Test caminho triste
@@ -80,19 +78,20 @@ public class AdsenseService implements IAdsenseService {
     /**
      * Nesse método retornamos uma lista de anúncio filtrado por produtos e Id
      * @param productId
-     * @return
      */
     @Override
     public List<AdsenseIdDto> findByProductId(Long productId) {
-        List<Adsense> adsenseList = findAll().stream().filter(a -> a.getProduct().getId().equals(productId))
-                .collect(Collectors.toList());
+        List<Adsense> adsenseList = findAll()
+            .stream()
+            .filter(a -> a.getProduct().getId().equals(productId))
+            .collect(Collectors.toList());
+
         return AdsenseIdDto.convertDto(adsenseList);
     }
 
     /**
      * Nesse método retornamos uma lista de anúncio e quantidade por armazém
      * @param adsenseId
-     * @return
      */
     @Override
     public List<AdsenseByWarehouseDto> findAdsenseByWarehouseAndQuantity(Long adsenseId) {
@@ -100,12 +99,11 @@ public class AdsenseService implements IAdsenseService {
     }
 
     /**
-     * <ul>
-     *     <li> Lista</li>
-     * </ul>
+     * Método responsável pela criação de um novo anúncio.
+     * Retorna alguns dados do anúncio criado.
+     * @param newAdsense Os dados do anúncio informados na requisção
+     * @return AdsenseInsertDto
      * @author Letícia Castro
-     * @param newAdsense
-     * @return
      */
     @Override
     public AdsenseInsertDto insertAdsense(Adsense newAdsense) {
@@ -120,9 +118,19 @@ public class AdsenseService implements IAdsenseService {
             .orElseThrow(() -> { throw new NotFound("Seller não cadastrado"); });
 
         adsenseRepository.save(newAdsense);
+
         return new AdsenseInsertDto(newAdsense, product, seller);
     }
 
+    /**
+     * Método responsável por atualizar um anúncio específico de um determinado vendedor.
+     * Retorna alguns dados do anúncio atualizado.
+     * @param adsenseId O ID do anúncio a ser pesquisado no banco.
+     * @param sellerId O ID do vendedor.
+     * @param adsense Os novos dados do anúncio.
+     * @return AdsenseUpdateDto
+     * @author Letícia Castro
+     */
     @Override
     public AdsenseUpdateDto updateAdsenseById(Adsense adsense, Long adsenseId, Long sellerId) {
         Adsense adsenseFound = findById(adsenseId);
@@ -141,6 +149,11 @@ public class AdsenseService implements IAdsenseService {
         return new AdsenseUpdateDto(adsenseFound, product);
     }
 
+    /**
+     * Método responsável por apagar um anúncio específico.
+     * @param id - ID do anúncio
+     * @author Letícia Castro
+     */
     @Override
     public void deleteAdsenseById(Long id) {
         Adsense adsense = findById(id);
@@ -151,3 +164,4 @@ public class AdsenseService implements IAdsenseService {
 }
 
 // TODO : ADSENSE: MELHORIAS: UPDATE SÓ É PERMITIDO SE NÃO HOUVE NENHUMA VENDA
+// TODO : ADSENSE: MELHORIAS: DELETE SÓ SER PERMITIDO SE O SELLER FOR O PROPRIETÁRIO
